87a91eb05ff0591974f0309ff80b5c4b2774d2ae
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-12-06 21:42:19 +0200

    finished proj-firstpart

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+}
+
+
+module.exports = {
+  images: {
+    domains: ["links.papareact.com", "a0.muscache.com", "ibb.co", "i.ibb.co"]
+  }
+}
