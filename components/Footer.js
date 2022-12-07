33e40e8e669ef34091e922844782f9a72bb936df
pacifiquem <pacifiquemurangwa001@gmail.com>
2022-12-07 08:24:42 +0200

    fin: finished first part

--- components/Footer.js
+++ components/Footer.js
@@ -4,36 +4,36 @@ function Footer() {
   return (
     <div className='grid grid-cols-1 md:grid-cols-4 gap-y-10 px-32 py-14 bg-gray-100 text-gray-600'>
         <div className='space-y-4 text-xs text-gray-800'>
-            <h5 className='font-bold'>ABOUT</h5>
-            <p className=' hover:cursor-pointer text-xl'>How Airbnb works</p>
-            <p className=' hover:cursor-pointer text-xl'>Newsroom</p>
-            <p className=' hover:cursor-pointer text-xl'>Investors</p>
-            <p className=' hover:cursor-pointer text-xl'>Airbnb Plus</p>
-            <p className=' hover:cursor-pointer text-xl'>Airbnb Luxe</p>
+            <h5 className='font-bold text-xl'>ABOUT</h5>
+            <p className=' hover:cursor-pointer text-[1rem]'>How Airbnb works</p>
+            <p className=' hover:cursor-pointer text-[1rem]'>Newsroom</p>
+            <p className=' hover:cursor-pointer text-[1rem]'>Investors</p>
+            <p className=' hover:cursor-pointer text-[1rem]'>Airbnb Plus</p>
+            <p className=' hover:cursor-pointer text-[1rem]'>Airbnb Luxe</p>
         </div>
         <div className='space-y-4 text-xs text-gray-800'>
-            <h5 className='font-bold'>ABOUT</h5>
-            <p className=' hover:cursor-pointer text-xl'>How Airbnb works</p>
-            <p className=' hover:cursor-pointer text-xl'>Newsroom</p>
-            <p className=' hover:cursor-pointer text-xl'>Investors</p>
-            <p className=' hover:cursor-pointer text-xl'>Airbnb Plus</p>
-            <p className=' hover:cursor-pointer text-xl'>Airbnb Luxe</p>
+            <h5 className='font-bold text-xl'>ABOUT</h5>
+            <p className=' hover:cursor-pointer text-[1rem]'>How Airbnb works</p>
+            <p className=' hover:cursor-pointer text-[1rem]'>Newsroom</p>
+            <p className=' hover:cursor-pointer text-[1rem]'>Investors</p>
+            <p className=' hover:cursor-pointer text-[1rem]'>Airbnb Plus</p>
+            <p className=' hover:cursor-pointer text-[1rem]'>Airbnb Luxe</p>
         </div>
         <div className='space-y-4 text-xs text-gray-800'>
-            <h5 className='font-bold'>ABOUT</h5>
-            <p className=' hover:cursor-pointer text-xl'>How Airbnb works</p>
-            <p className=' hover:cursor-pointer text-xl'>Newsroom</p>
-            <p className=' hover:cursor-pointer text-xl'>Investors</p>
-            <p className=' hover:cursor-pointer text-xl'>Airbnb Plus</p>
-            <p className=' hover:cursor-pointer text-xl'>Airbnb Luxe</p>
+            <h5 className='font-bold text-xl'>ABOUT</h5>
+            <p className=' hover:cursor-pointer text-[1rem]'>How Airbnb works</p>
+            <p className=' hover:cursor-pointer text-[1rem]'>Newsroom</p>
+            <p className=' hover:cursor-pointer text-[1rem]'>Investors</p>
+            <p className=' hover:cursor-pointer text-[1rem]'>Airbnb Plus</p>
+            <p className=' hover:cursor-pointer text-[1rem]'>Airbnb Luxe</p>
         </div>
         <div className='space-y-4 text-xs text-gray-800'>
-            <h5 className='font-bold'>ABOUT</h5>
-            <p className=' hover:cursor-pointer text-xl'>How Airbnb works</p>
-            <p className=' hover:cursor-pointer text-xl'>Newsroom</p>
-            <p className=' hover:cursor-pointer text-xl'>Investors</p>
-            <p className=' hover:cursor-pointer text-xl'>Airbnb Plus</p>
-            <p className=' hover:cursor-pointer text-xl'>Airbnb Luxe</p>
+            <h5 className='font-bold text-xl'>ABOUT</h5>
+            <p className=' hover:cursor-pointer text-[1rem]'>How Airbnb works</p>
+            <p className=' hover:cursor-pointer text-[1rem]'>Newsroom</p>
+            <p className=' hover:cursor-pointer text-[1rem]'>Investors</p>
+            <p className=' hover:cursor-pointer text-[1rem]'>Airbnb Plus</p>
+            <p className=' hover:cursor-pointer text-[1rem]'>Airbnb Luxe</p>
         </div>
     </div>
   )
